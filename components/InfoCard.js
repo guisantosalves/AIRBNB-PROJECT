7decb19fbda992ecf127c279e5bfca4cd156a020
guisantosalves <workguisantos@gmail.com>
2022-10-20 11:20:04 -0400

    search page is done

---
+++ components/InfoCard.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Image from "next/image";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/solid";
+
+const InfoCard = ({
+  img,
+  location,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) => {
+  return (
+    <div className="flex py-7 px-2 pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
+      <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
+        <Image src={img} layout="fill" objectFit="cover" className="rounded-2xl"/>
+      </div>
+
+      <div className="flex flex-col flex-grow pl-5">
+        {/* justify-between -> justify-content */}
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 w-7 cursor-pointer" />
+        </div>
+        <h4 className="text-xl">{title}</h4>
+        <div className="border-b w-10 pt-2" /> {/* the line */}
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+        <div className="flex justify-between items-end">
+          <p className="flex items-center">
+            <StarIcon className="h-5 w-5 text-red-400" />
+            {star}
+          </p>
+
+          <div>
+            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
--- pages/search.js
+++ pages/search.js
@@ -5,25 +5,31 @@ import format from "date-fns/format";
 // components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import InfoCard from "../components/InfoCard";
 
+// we can pass the object from the getServerSideProps
 // the component needs the uppercase, the file doesn't
-const Search = () => {
+const Search = ({ searchResults }) => {
   const router = useRouter();
 
   // es6 destruction
-  const {location, startDate, endDate, noOfGuests} = router.query;
+  const { location, startDate, endDate, noOfGuests } = router.query;
 
   const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
   const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
   const rangeOfDates = `${formattedStartDate} - ${formattedEndDate}`;
-
+  console.log(searchResults)
   return (
     <div className="h-screen">
-      <Header placeholder={`${location} | ${rangeOfDates} | ${noOfGuests}`}/>
+      <Header placeholder={`${location} | ${rangeOfDates} | ${noOfGuests}`} />
       <main className="flex">
         <section className="flex-grow pt-14 px-6 ">
-          <p className="text-xs">300+ Stays - {rangeOfDates} - for {noOfGuests} guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+          <p className="text-xs">
+            300+ Stays - {rangeOfDates} - for {noOfGuests} guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
           {/* Sequences of whitespace will collapse into a single whitespace. Text will never wrap to the next line.
                     The text continues on the same line until a <br> tag is encountered */}
@@ -37,6 +43,24 @@ const Search = () => {
             <p className="button">Rooms and Beds</p>
             <p className="button">More filters</p>
           </div>
+
+          <div className="flex flex-col">
+            {searchResults.map((item, index) => {
+              return (
+                <InfoCard
+                  key={index}
+                  img={item.img}
+                  location={item.location}
+                  title={item.title}
+                  description={item.description}
+                  star={item.star}
+                  price={item.price}
+                  total={item.total}
+                />
+              );
+            })}
+          </div>
+
         </section>
       </main>
       <Footer />
@@ -45,3 +69,16 @@ const Search = () => {
 };
 
 export default Search;
+
+// to do the request in next js
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
